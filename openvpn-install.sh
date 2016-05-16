ef1ae85797fa4d1bf456adff45f759e169c4bf8d
Nyr <me@nyr.be>
2016-05-16 02:52:33 +0200

    Change cipher to AES-128-CBC

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -276,6 +276,7 @@ ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
 		;;
 	esac
 	echo "keepalive 10 120
+cipher AES-128-CBC
 comp-lzo
 user nobody
 group $GROUPNAME
@@ -374,6 +375,7 @@ nobind
 persist-key
 persist-tun
 remote-cert-tls server
+cipher AES-128-CBC
 comp-lzo
 setenv opt block-outside-dns
 key-direction 1
