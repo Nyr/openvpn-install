e364e52d67201b67b718317e7040cc9d3fa74016
Diveyez <nemofbaltimore@gmail.com>
2018-03-10 20:38:01 -0500

    Additions to repo
    Added a connection helper script. Made changes to README

---
+++ connect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# OPENVPN OTG CONNECT SCRIPT
+echo "Where is the .ovpn config file?: (/path/to/file.ovpn)"
+read $fileloc
+sudo screen -dmS ovpn openvpn --client $fileloc
+echo "Done, you should be connected, to end the connection, screen -x ovpn and ctrl+c"
