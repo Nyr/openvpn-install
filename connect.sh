6e5bc83d565ab5b7191808355ff2c945fdb92eac
Diveyez <nemofbaltimore@gmail.com>
2018-03-12 01:59:16 -0400

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -2,5 +2,5 @@
 # OPENVPN OTG CONNECT SCRIPT
 echo "Where is the .ovpn config file?: (/path/to/file.ovpn)"
 read fileloc
-sudo screen -dmS ovpn openvpn --config $fileloc
+sudo screen -d -m -S ovpn openvpn --config $fileloc
 echo "Done, you should be connected, to end the connection, screen -x ovpn and ctrl+c"
