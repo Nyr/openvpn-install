589509a665ae9e70546ce35c17d97eb69554147d
Daniel Stinebaugh <daniel@stinebaugh.info>
2018-03-14 10:01:28 -0700

    Added the option for Quad9 DNS
    Adding option 7 to include the Fast and Secure DNS offering from
    quad9.net

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -212,6 +212,7 @@ else
 	echo "   4) NTT"
 	echo "   5) Hurricane Electric"
 	echo "   6) Verisign"
+	echo "   7) Quad 9"
 	read -p "DNS [1-6]: " -e -i 1 DNS
 	echo ""
 	echo "Finally, tell me your name for the client certificate"
@@ -302,6 +303,9 @@ ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
 		6) 
 		echo 'push "dhcp-option DNS 64.6.64.6"' >> /etc/openvpn/server.conf
 		echo 'push "dhcp-option DNS 64.6.65.6"' >> /etc/openvpn/server.conf
+		;;
+		5) 
+		echo 'push "dhcp-option DNS 9.9.9.9"' >> /etc/openvpn/server.conf
 		;;
 	esac
 	echo "keepalive 10 120
