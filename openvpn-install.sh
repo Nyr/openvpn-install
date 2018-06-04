7dc2ef433d02310886ea8ad7afc25cf9d3345b29
Christian Berkman <christian@nacmu.org>
2018-06-04 15:18:14 +0300

    Added an extra \ on line 74 by mistake

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -71,7 +71,7 @@ if [[ -e /etc/openvpn/server.conf ]]; then
 			echo "Please, use one word only, no special characters."
 			read -p "Client name: " -e CLIENT
 			cd /etc/openvpn/easy-rsa/
-			./easyrsa build-client-full $CLIENT nopass\
+			./easyrsa build-client-full $CLIENT nopass
 			# Ask to set a passphrase for the configuration file
 			read -p "Do you want to set a passphrase for the configuration file? [y/N]: " -e -i N SETPASS
 			if [[ "$SETPASS" = 'y' || "$SETPASS" = 'Y' ]]; then
