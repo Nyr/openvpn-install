Nyr/openvpn-install

commit b3953963bac3ef048ffd09d7dcb9992eea383acb

Switch from /etc/sysctl.conf to systemd-sysctl

openvpn-install.sh
@@ -154,6 +154,7 @@ if [[ -e /etc/openvpn/server.conf ]]; then
 					yum remove openvpn -y
 				fi
 				rm -rf /etc/openvpn
+				rm -f /etc/sysctl.d/30-openvpn-forward.conf
 				echo
 				echo "OpenVPN removed!"
 			else
@@ -304,11 +305,8 @@ status openvpn-status.log
 verb 3
 crl-verify crl.pem" >> /etc/openvpn/server.conf
 	# Enable net.ipv4.ip_forward for the system
-	sed -i '/\<net.ipv4.ip_forward\>/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
-	if ! grep -q "\<net.ipv4.ip_forward\>" /etc/sysctl.conf; then
-		echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-	fi
-	# Avoid an unneeded reboot
+	echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-openvpn-forward.conf
+	# Enable without waiting for a reboot or service restart
 	echo 1 > /proc/sys/net/ipv4/ip_forward
 	if pgrep firewalld; then
 		# Using both permanent and not permanent rules to avoid a firewalld

commit 510f9e1bf8b9a681d224299ee7c82953752555ef

Remove support for old init systems
It was broken since b3953963bac3ef048ffd09d7dcb9992eea383acb anyway.

openvpn-install.sh
@@ -358,21 +358,9 @@ exit 0' > $RCLOCAL
 		semanage port -a -t openvpn_port_t -p $PROTOCOL $PORT
 	fi
 	# And finally, restart OpenVPN
-	if [[ "$OS" = 'debian' ]]; then
-		# Little hack to check for systemd
-		if pgrep systemd-journal; then
-			systemctl restart openvpn@server.service
-		else
-			/etc/init.d/openvpn restart
-		fi
-	else
-		if pgrep systemd-journal; then
-			systemctl restart openvpn@server.service
-			systemctl enable openvpn@server.service
-		else
-			service openvpn restart
-			chkconfig openvpn on
-		fi
+	systemctl restart openvpn@server.service
+	if [[ "$OS" = 'centos' ]]; then
+		systemctl enable openvpn@server.service
 	fi
 	# If the server is behind a NAT, use the correct IP address
 	if [[ "$PUBLICIP" != "" ]]; then