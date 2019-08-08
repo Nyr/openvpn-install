3c50a2bbc97aada96dc5570162bab1891f1030ad
Matthew Frost <m.frost@mattronix.nl>
2019-08-08 14:27:54 +0200

    fixed my error using wrong var

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -178,8 +178,10 @@ else
 	# If $IP is a private IP address, the server must be behind NAT
 	if echo "$IP" | grep -qE '^(10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|192\.168)'; then
 	        # Autodetect IP address and pre-fill for the user
-	        IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-		read -p "IP address: " -e -i $IP IP
+	        PUBLICIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+		echo "This server is behind NAT. What is the public IPv4 address or hostname?"
+		read -p "Public IP address / hostname: " -e PUBLICIP
+
 	fi
 	echo
 	echo "Which protocol do you want for OpenVPN connections?"
