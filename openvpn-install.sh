c0d583bc3478cbe35566cf9b89808e352a32fb4f
Leonard Wong <me@leonardwong.net>
2021-04-06 23:28:06 +0800

    Updated DNS Options
    Added option for 1.1.1.2 or 1.1.1.3

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -179,9 +179,11 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 	echo "   1) Current system resolvers"
 	echo "   2) Google"
 	echo "   3) 1.1.1.1"
-	echo "   4) OpenDNS"
-	echo "   5) Quad9"
-	echo "   6) AdGuard"
+	echo "   4) 1.1.1.2 (Block Malware)"
+	echo "   5) 1.1.1.3 (Block Malware & Adult Content)"
+	echo "   6) OpenDNS"
+	echo "   7) Quad9"
+	echo "   8) AdGuard"
 	read -p "DNS server [1]: " dns
 	until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
 		echo "$dns: invalid selection."
@@ -302,14 +304,22 @@ server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
 			echo 'push "dhcp-option DNS 1.0.0.1"' >> /etc/openvpn/server/server.conf
 		;;
 		4)
+			echo 'push "dhcp-option DNS 1.1.1.2"' >> /etc/openvpn/server/server.conf
+			echo 'push "dhcp-option DNS 1.0.0.2"' >> /etc/openvpn/server/server.conf
+		;;
+		5)
+			echo 'push "dhcp-option DNS 1.1.1.3"' >> /etc/openvpn/server/server.conf
+			echo 'push "dhcp-option DNS 1.0.0.3"' >> /etc/openvpn/server/server.conf
+		;;
+		6)
 			echo 'push "dhcp-option DNS 208.67.222.222"' >> /etc/openvpn/server/server.conf
 			echo 'push "dhcp-option DNS 208.67.220.220"' >> /etc/openvpn/server/server.conf
 		;;
-		5)
+		7)
 			echo 'push "dhcp-option DNS 9.9.9.9"' >> /etc/openvpn/server/server.conf
 			echo 'push "dhcp-option DNS 149.112.112.112"' >> /etc/openvpn/server/server.conf
 		;;
-		6)
+		8)
 			echo 'push "dhcp-option DNS 94.140.14.14"' >> /etc/openvpn/server/server.conf
 			echo 'push "dhcp-option DNS 94.140.15.15"' >> /etc/openvpn/server/server.conf
 		;;
