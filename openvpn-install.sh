2d58be7da42e3ecdc4d4d163b70d7e41f7c54e4a
simopunkc <pembela.allah@gmail.com>
2022-08-17 22:22:35 +0700

    Add support for Kali Rolling 2022

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -26,6 +26,10 @@ if grep -qs "ubuntu" /etc/os-release; then
 	os="ubuntu"
 	os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
 	group_name="nogroup"
+elif grep -qs "kali" /etc/os-release; then
+	os="debian"
+	os_version=11
+	group_name="nogroup"
 elif [[ -e /etc/debian_version ]]; then
 	os="debian"
 	os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
