e93f409a7d8c30da3e0c56079a132cfba71c149c
MohamadSafi <the.mohammad.safi@gmail.com>
2023-09-25 12:12:05 +0300

    Changing Subnet

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -276,7 +276,7 @@ dh dh.pem
 auth SHA512
 tls-crypt tc.key
 topology subnet
-server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
+server 10.8.0.0 255.255.255.128" > /etc/openvpn/server/server.conf
 	# IPv6
 	if [[ -z "$ip6" ]]; then
 		echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server/server.conf
