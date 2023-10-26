52b12468b1b70ce349a46a7598034a0931e4d5d7
Nyr <me@nyr.be>
2023-10-26 16:11:50 +0200

    Remove --cipher parameter
    --cipher has been deprecated since v2.4 and was kept for compatibility purposes.

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -329,7 +329,6 @@ server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
 	esac
 	echo 'push "block-outside-dns"' >> /etc/openvpn/server/server.conf
 	echo "keepalive 10 120
-cipher AES-256-CBC
 user nobody
 group $group_name
 persist-key
@@ -429,7 +428,6 @@ persist-key
 persist-tun
 remote-cert-tls server
 auth SHA512
-cipher AES-256-CBC
 ignore-unknown-option block-outside-dns
 verb 3" > /etc/openvpn/server/client-common.txt
 	# Enable and start the OpenVPN service
