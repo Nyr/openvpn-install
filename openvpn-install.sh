e57407420c502a5ff99d24218e9ac2c6dad62805
Nyr <me@nyr.be>
2025-03-10 20:44:36 +0100

    Cleanup leftover files after client revocation
    Closes #1134

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -519,6 +519,8 @@ else
 				./easyrsa --batch revoke "$client"
 				./easyrsa --batch --days=3650 gen-crl
 				rm -f /etc/openvpn/server/crl.pem
+				rm -f /etc/openvpn/server/easy-rsa/pki/reqs/"$client".req
+				rm -f /etc/openvpn/server/easy-rsa/pki/private/"$client".key
 				cp /etc/openvpn/server/easy-rsa/pki/crl.pem /etc/openvpn/server/crl.pem
 				# CRL is read with each client connection, when OpenVPN is dropped to nobody
 				chown nobody:"$group_name" /etc/openvpn/server/crl.pem
