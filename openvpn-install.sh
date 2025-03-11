e30c8a38e73fe3f6821a8c6a3cb5258c83ca2d24
Nyr <me@nyr.be>
2025-03-11 19:47:39 +0100

    Use a better client configuration file path

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -80,6 +80,9 @@ TUN needs to be enabled before running this installer."
 	exit
 fi
 
+# Store the absolute path of the directory where the script is located
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 new_client () {
 	# Generates the custom client.ovpn
 	{
@@ -96,7 +99,7 @@ new_client () {
 	echo "<tls-crypt>"
 	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
 	echo "</tls-crypt>"
-	} > ~/"$client".ovpn
+	} > "$script_dir"/"$client".ovpn
 }
 
 if [[ ! -e /etc/openvpn/server/server.conf ]]; then
@@ -456,7 +459,7 @@ verb 3" > /etc/openvpn/server/client-common.txt
 	echo
 	echo "Finished!"
 	echo
-	echo "The client configuration is available in:" ~/"$client.ovpn"
+	echo "The client configuration is available in:" "$script_dir"/"$client.ovpn"
 	echo "New clients can be added by running this script again."
 else
 	clear
@@ -488,7 +491,7 @@ else
 			# Generates the custom client.ovpn
 			new_client
 			echo
-			echo "$client added. Configuration available in:" ~/"$client.ovpn"
+			echo "$client added. Configuration available in:" "$script_dir"/"$client.ovpn"
 			exit
 		;;
 		2)
